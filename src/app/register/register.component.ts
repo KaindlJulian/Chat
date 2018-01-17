ea40d9500e489df7ebcc6b15b3e139e04ec311a3
LAPTOP\Julian <kaindl745@gmail.com>
2018-01-17 12:07:46 +0100

    jwt

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../_services/users.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  model: any = {}
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
+    this.authenticationService.logout();
+  }
+
+  register() {
+    this.authenticationService.register(model.name, model.email, model.username, model.password)
+    .subscribe(result => {
+        if (result === true) {
+            // succesfull register
+            this.router.navigate(['/']);
+        } else {
+            this.router.navigate(['/']);
+        }
+    });
   }
 
 }
