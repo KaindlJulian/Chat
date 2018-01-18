3bd759c6f93d69d92ce74b862ff59791c7464778
LAPTOP\Julian <kaindl745@gmail.com>
2018-01-18 15:24:00 +0100

    succ

--- src/app/_services/socket.service.ts
+++ src/app/_services/socket.service.ts
@@ -4,11 +4,11 @@ import { Observer } from 'rxjs/Observer';
 
 import { User } from '../_models/user';
 import { Message } from '../_models/messages';
-import { Event } from '../_models/event';
+// import { Event } from '../_models/event';
 
 import * as io from 'socket.io-client';
 
-const SERVER_URL = 'http://localhost:3000';
+const SERVER_URL = 'http://localhost:3080';
 const JWT_TOKEN = localStorage.getItem('currentUser');
 
 @Injectable()
@@ -19,25 +19,33 @@ export class SocketService {
     public initSocket(): void {
         this.socket = io.connect(SERVER_URL ,
             {'extraHeaders': { Authorization: 'Bearer ' + JWT_TOKEN }}
-        );    
-    }
-
-    public createChat(name: string, users: User[]): void{
-        socket.emit('createChat', ({name: name, user: users}));
-    }
-
-    public onEvent(event: Event): Observable: any {
-        return new Observable<Event>(observer => {
-            this.socket.on(event, () => observer.next());
-        });
+        );
     }
 
     public disconnect(): void {
         socket.disconnect();
     }
 
-    public sendMessage(msg: Message): void{
-        socket.emit('sendMessage',({
+    public onSuccsess(): Observable<Group> {
+        return new Observable<Group>(observer => {
+            this.socket.on('success', (data: Group) => observer.next(data));
+        });
+    }
+
+    public onWorks(): Observable<String> {
+        return new Observable<String>(observer => {
+            this.socket.on('works',(data: String) => observer.next(data));
+        });
+    }
+
+
+
+    public createChat(name: string, users: User[]): void {
+        socket.emit('createChat', ({name: name, user: users}));
+    }
+
+    public sendMessage(msg: Message): void {
+        socket.emit('sendMessage', ({
             sender_id : msg.sender_id,
             receiver_id : msg.receiver_id,
             msg : msg.msg,
@@ -45,4 +53,10 @@ export class SocketService {
         }));
     }
 
+
+    /*public onEvent(event: Event): Observable: any {
+        return new Observable<Event>(observer => {
+            this.socket.on(event, () => observer.next());
+        });
+    }*/
 }
